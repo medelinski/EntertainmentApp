e9a673bd63208c158b2f86721b6d5c097c366f46
medelinski8813@conestogac.on.ca <medelinski8813@conestogac.on.ca>
2018-03-15 17:56:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/medelinski8813/entertainmentapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.medelinski8813.entertainmentapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnLogin;
+    private Button btnRegister;
+    private EditText etUsername;
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnLogin:
+                //Link to homepage
+             break;
+            case R.id.btnRegister:
+                //Link to register
+                break;
+
+
+        }
+    }
+}
